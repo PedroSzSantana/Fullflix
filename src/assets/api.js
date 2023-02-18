1e4d6a40b5753e2b4424761ec7436dcc5458a0c3
Pedro Souza <108759317+PedroSzSantana@users.noreply.github.com>
2023-02-18 00:16:41 -0300

    Update api.js

--- src/assets/api.js
+++ src/assets/api.js
@@ -1,17 +1,14 @@
 import axios from 'axios'
 
-export const getList = (setState)=>{
-    axios.get("https://api.themoviedb.org/3/movie/popular?api_key=67c1b764b81ffc56216aaf91692930d2&language=pt-BR&page=1")
-    .then((response)=>{
-        setState(response.data.results)
-    }).catch((error)=>{
-        console.log(error)
-    })
+export const getList = async(setState)=>{
+    const response = await axios.get("https://api.themoviedb.org/3/movie/popular?api_key=67c1b764b81ffc56216aaf91692930d2&language=pt-BR&page=1")
+
+    setState(response.data.results)
+
 }
 export const getDetails = async(id, setState)=>{
     const response = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=67c1b764b81ffc56216aaf91692930d2&language=pt-BR`)
-      setState(response.data)
-    }).catch((error)=>{
-        console.log(error)
-    })
+
+    setState(response.data)
+
 }
